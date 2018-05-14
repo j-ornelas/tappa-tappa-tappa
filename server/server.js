db0b9d7cb1de3f3cc3ed9f0420e8fd924beda933
j-ornelas <johnnyoh1988@gmail.com>
2018-05-13 19:13:02 -0700

    Ver. 1.0

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const port = 4200;
-const title =  `Application`;
+const title =  `TappaTappaTappa`;
 const path = require('path');
 const bodyParser = require('body-parser');
 
